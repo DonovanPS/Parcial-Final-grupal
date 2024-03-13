3a3abce70c6cba78ebb933aee0049ada24b60589
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2024-03-12 19:40:17 -0500

    Consumo

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,49 +1,75 @@
-// resolvers/resolvers.js
-
-const axios = require('axios');
+const axios = require("axios");
 
 const resolvers = {
   Query: {
     getAllProducts: async () => {
       try {
-        const response = await axios.get('https://api-rest-julieth-donovan.onrender.com/products');
-        return response.data.data; // Suponiendo que la respuesta tiene un campo 'data' que contiene los productos
+        const response = await axios.get(
+          "https://api-rest-julieth-donovan.onrender.com/products"
+        );
+        const data = response.data.data;
+        return data.map((product) => ({
+          id: product._id,
+          name: product.name,
+          description: product.description,
+          price: product.price,
+          stock: product.stock,
+          category: product.category
+            ? {
+                id: product.category._id,
+                name: product.category.name,
+                description: product.category.description,
+              }
+            : null,
+        }));
       } catch (error) {
-        throw new Error('Error al obtener todos los productos');
+        throw new Error("No se pudieron obtener los productos.");
       }
     },
     getProductById: async (_, { id }) => {
       try {
-        const response = await axios.get(`https://api-rest-julieth-donovan.onrender.com/products/${id}`);
-        return response.data.data; // Suponiendo que la respuesta tiene un campo 'data' que contiene el producto
+        const response = await axios.get(
+          `https://api-rest-julieth-donovan.onrender.com/products/${id}`
+        );
+        const product = response.data.data;
+        product.id = product._id;
+        return product;
       } catch (error) {
-        throw new Error('Error al obtener el producto por ID');
+        throw new Error("Error al obtener el producto por ID");
       }
     },
   },
   Mutation: {
     addProduct: async (_, { input }) => {
       try {
-        const response = await axios.post('https://api-rest-julieth-donovan.onrender.com/products', input);
+        const response = await axios.post(
+          "https://api-rest-julieth-donovan.onrender.com/products",
+          input
+        );
         return response.data.data; // Suponiendo que la respuesta tiene un campo 'data' que contiene el producto guardado
       } catch (error) {
-        throw new Error('Error al agregar el producto');
+        throw new Error("Error al agregar el producto");
       }
     },
     updateProduct: async (_, { id, input }) => {
       try {
-        const response = await axios.put(`https://api-rest-julieth-donovan.onrender.com/products/${id}`, input);
+        const response = await axios.put(
+          `https://api-rest-julieth-donovan.onrender.com/products/${id}`,
+          input
+        );
         return response.data.data; // Suponiendo que la respuesta tiene un campo 'data' que contiene el producto actualizado
       } catch (error) {
-        throw new Error('Error al actualizar el producto');
+        throw new Error("Error al actualizar el producto");
       }
     },
     deleteProduct: async (_, { id }) => {
       try {
-        await axios.delete(`https://api-rest-julieth-donovan.onrender.com/products/${id}`);
+        await axios.delete(
+          `https://api-rest-julieth-donovan.onrender.com/products/${id}`
+        );
         return true;
       } catch (error) {
-        throw new Error('Error al eliminar el producto');
+        throw new Error("Error al eliminar el producto");
       }
     },
   },
